f2fb6403d9fc5cc64255199584143e0bfd35165a
MohamedElgamasy84 <mohamedelgamasy43@gmail.com>
2025-12-16 10:53:58 +0200

    ..

--- BL/Models/SalesOrderDetail.cs
+++ BL/Models/SalesOrderDetail.cs
@@ -1,6 +1,7 @@
 ﻿using BL.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ namespace Core.Models
         public int ProductId { get; set; }
 
         public int Quantity { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+
         public decimal UnitPrice { get; set; }
         public decimal? Discount { get; set; }
         public int ProductUnitId { get; set; }
